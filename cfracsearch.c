fe82075d79549b5c248cd24dbe67258aeb38ee0f
JamesRickards-Canada <james.rickards@colorado.edu>
2023-12-31 14:17:29 -0400

    Updated cfracsearch to use unsigned ints, 1<<v instead of bitswap, and (by far most importantly) use atomic operations. The atomicity means adding more threads only adds small amounts of memory, hence this scales MUCH better with more cores than before. No more memory problems!

--- cfracsearch.c
+++ cfracsearch.c
@@ -7,10 +7,11 @@
 
 #include <dirent.h>
 #include <errno.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <pthread.h>
+#include <stdatomic.h>
 #include <time.h>
 
 typedef struct _cfrac_t {/*[0;4a_n,...,4a_1,k,1,2]. The threads start with either [4a_1, k, 1, 2] or [k, 1, 2]*/
@@ -18,7 +19,7 @@ typedef struct _cfrac_t {/*[0;4a_n,...,4a_1,k,1,2]. The threads start with eithe
   long Bmin;
   long Bmax;
   int Nthreads;
-  unsigned long *found;/*The found denominators. We actually do this locally to avoid mutexing a ton.*/
+  atomic_uint *found;/*The found denominators, using atomic rather than mutexing since it is done so much.*/
   /*We want to break up the first few values of k since they really dominate (we don't want 1 thread still running while the others are all done). Similarly, after a certain point, we should just assign each thread to look at that class modulo Nthreads, since the continual mutexing is a lot of wastage. */
   long Ninitial;/*The number of precomputed numerator and denominator pairs to run through.*/
   unsigned long *den0;/*The vector of initial den[0]'s*/
@@ -30,19 +31,19 @@ typedef struct _cfrac_t {/*[0;4a_n,...,4a_1,k,1,2]. The threads start with eithe
 
 pthread_mutex_t mutex_cfrac;
 
-static void missing_tofile(unsigned long *found, long nblocks, long Bmin, long Bmax);
+static void missing_tofile(atomic_uint *found, long nblocks, long Bmin, long Bmax);
 static void *cfrac_par(void *args);
 
 
 /*Updates found with N*/
-inline void
-found_update(unsigned long *found, unsigned long *bitswap, long Bmin, long N)
+static inline void
+found_update(atomic_uint *found, long Bmin, long N)
 {
   long shifted = N - Bmin;
   if (shifted < 0) return;
-  long v = shifted % 64;
-  long u = shifted >> 6;/*shifted = 64*u+v. u gives the entry of the array, v gives the bit to swap.*/
-  found[u] |= bitswap[v];
+  long v = shifted % 32;
+  long u = shifted >> 5;/*shifted = 32*u+v. u gives the entry of the array, v gives the bit to swap.*/
+  found[u] |= ((unsigned int) 1) << v;
 }
 
 /*Computes all missing denominators between Bmin and Bmax (inclusive), storing them to a file.*/
@@ -56,21 +57,18 @@ main(int argc, char *argv[])
   long Bmax = atol(argv[2]);
   int Nthreads = atoi(argv[3]);
   long Bdiff = Bmax - Bmin + 1, i;
-  long nblocks = ((Bdiff - 1) >> 6) + 1;
-  unsigned long *found = (unsigned long *)calloc(nblocks, sizeof(unsigned long));/*Stores the found denominators*/
+  long nblocks = ((Bdiff - 1) >> 5) + 1;
+  atomic_uint *found = (atomic_uint *)calloc(nblocks, sizeof(atomic_uint));/*Stores the found denominators*/
   if (!found) {printf("Insufficient memory to store the denominators found.\n"); exit(1); }
-  unsigned long *bitswap = (unsigned long *)malloc(sizeof(unsigned long) << 6);/*Used for swapping bits of unsigned longs. ASSUME 64 BIT SYSTEM.*/
-  bitswap[0] = 1;
-  for (i = 1; i < 64; i++) bitswap[i] = bitswap[i - 1] << 1;/*bitswap[i] = 2^i*/
   long Ninitial = (Nthreads + 5);
   Ninitial = Ninitial * Ninitial ;/*Go Nthreads + 5 deep on the first 2.*/
   unsigned long *den0 = (unsigned long *)malloc(Ninitial * sizeof(unsigned long));
   unsigned long *den1 = (unsigned long *)malloc(Ninitial * sizeof(unsigned long));
   long k, a1, a2, ind = 0;
-  if (3 <= Bmax) found_update(found, bitswap, Bmin, 3);/*Denominator of 3*/
+  if (3 <= Bmax) found_update(found, Bmin, 3);/*Denominator of 3*/
   for (k = 1; k <= Nthreads + 5; k++) {/*Let's go a two levels deep for the first few entries.*/
     long d = 3 * k + 2;/*The denominator*/
-    if (d <= Bmax) found_update(found, bitswap, Bmin, d);/*We add the first 1 only, no need for the second since that is done in the thread.*/
+    if (d <= Bmax) found_update(found, Bmin, d);/*We add the first 1 only, no need for the second since that is done in the thread.*/
     for (a1 = 1; a1 <= Nthreads + 5; a1++) {
       long e = (a1 << 2) * d + 3;
       den0[ind] = d;/*Add 'em in!*/
@@ -78,7 +76,6 @@ main(int argc, char *argv[])
       ind++;
     }
   }
-  free(bitswap);
   pthread_t thread_id[Nthreads];/*The thread ids*/
   cfrac_t data[Nthreads];
   long initialdentodo = 0, kdentodo = Nthreads + 6;/*We did up to Nthreads + 5 in the first three levels.*/
@@ -113,7 +110,7 @@ main(int argc, char *argv[])
 
 /*Prints the found data to a file.*/
 static void
-missing_tofile(unsigned long *found, long nblocks, long Bmin, long Bmax)
+missing_tofile(atomic_uint *found, long nblocks, long Bmin, long Bmax)
 {
   char fname[200];
   int pos = 0;
@@ -134,21 +131,21 @@ missing_tofile(unsigned long *found, long nblocks, long Bmin, long Bmax)
   F = fopen(fname, "w");
   long u;
   for (u = 0; u < nblocks - 1; u++) {/*Figure out each block.*/
-    unsigned long val = found[u];
+    unsigned int val = found[u];
     long v;
-    for (v = 0; v < 64; v++) {
+    for (v = 0; v < 32; v++) {
       if (!(val & 1)) {/*A missing value*/
-        long n = (u << 6) + v + Bmin;/*The value.*/
+        long n = (u << 5) + v + Bmin;/*The value.*/
         fprintf(F, "%ld\n", n);/*Print it to the file.*/
       }
       val >>= 1;/*Shift right one.*/
     }
   }
-  unsigned long val = found[nblocks - 1];/*Final block: we need to make sure we don't go back Bmax here.*/
+  unsigned int val = found[nblocks - 1];/*Final block: we need to make sure we don't go back Bmax here.*/
   long v;
-  for (v = 0; v < 64; v++) {
+  for (v = 0; v < 32; v++) {
     if (!(val & 1)) {/*A missing value*/
-      long n = (u << 6) + v + Bmin;/*The value.*/
+      long n = (u << 5) + v + Bmin;/*The value.*/
       if (n <= Bmax) fprintf(F, "%ld\n", n);/*Print it to the file.*/
       else break;/*All done!*/
     }
@@ -162,14 +159,10 @@ static void *
 cfrac_par(void *args)
 {
   cfrac_t *data = (cfrac_t *)args;/*Import the given data.*/
+  atomic_uint *found = data -> found;
   long Bmin = data -> Bmin, Bmax = data -> Bmax;
   int Nthreads = data -> Nthreads, Ninitial = data -> Ninitial;
-  unsigned long *bitswap = (unsigned long *)malloc(sizeof(unsigned long) << 6), i;/*Used for swapping bits of unsigned longs. ASSUME 64 BIT SYSTEM.*/
-  bitswap[0] = 1;
-  for (i = 1; i < 64; i++) bitswap[i] = bitswap[i - 1] << 1;/*bitswap[i] = 2^i*/
-  long nblocks = ((Bmax - Bmin) >> 6) + 1;
-  unsigned long *localfound = (unsigned long *)calloc(nblocks, sizeof(unsigned long));/*Local version of found to avoid mutexing a lot.*/
-  if (!localfound) {printf("Insufficient memory to store the denominators (in the thread) found.\n"); exit(1); }
+  long nblocks = ((Bmax - Bmin) >> 5) + 1;
   long maxdepth = 100;/*Maximal depth, to start.*/
   unsigned long *dens = (unsigned long *)malloc(maxdepth * sizeof(unsigned long));/*Tracks the sequence of denominators.*/
   if (!dens) { printf("Insufficient memory to store the denominator sequence.\n"); exit(1); }
@@ -193,7 +186,7 @@ cfrac_par(void *args)
     }
     pthread_mutex_unlock(&mutex_cfrac);/*Unlock it.*/
     if (dens[1] > Bmax) continue;/*Nothing to do here.*/
-    found_update(localfound, bitswap, Bmin, dens[1]);/*Add the first denominator.*/
+    found_update(found, Bmin, dens[1]);/*Add the first denominator.*/
     long ind = 2;
     dens[2] = dens[0];/*Initially set to this.*/ 
     long baseind = cont? 0 : 1;/*If cont = 1, continue to change index 1. Else, only do index 2 and onward.*/
@@ -203,7 +196,7 @@ cfrac_par(void *args)
         ind--;/*Back up.*/
         continue;
       }
-      found_update(localfound, bitswap, Bmin, newden);/*Update the saved missing denominators.*/
+      found_update(found, Bmin, newden);/*Update the saved missing denominators.*/
       dens[ind] = newden;/*Update dens.*/
       ind++;
       if (ind == maxdepth) {/*We are going too deep, must reallocate the storage location.*/
@@ -224,7 +217,7 @@ cfrac_par(void *args)
   dens[0] = 3;
   unsigned long threeNthreads = 3 * Nthreads;
   while (dens[1] <= Bmax) {/*Where we execute the search in arithmetic progressions of k modulo Nthreads.*/
-    found_update(localfound, bitswap, Bmin, dens[1]);/*Add the first denominator.*/
+    found_update(found, Bmin, dens[1]);/*Add the first denominator.*/
     long ind = 2;
     dens[2] = dens[0];/*Initially set to this.*/ 
     while (ind > 1) {
@@ -233,7 +226,7 @@ cfrac_par(void *args)
         ind--;/*Back up.*/
         continue;
       }
-      found_update(localfound, bitswap, Bmin, newden);/*Update the saved missing denominators.*/
+      found_update(found, Bmin, newden);/*Update the saved missing denominators.*/
       dens[ind] = newden;/*Update dens.*/
       ind++;
       if (ind == maxdepth) {/*We are going too deep, must reallocate the storage location.*/
@@ -248,12 +241,7 @@ cfrac_par(void *args)
     }
     dens[1] += threeNthreads;/*Increment along the sequence.*/
   }
-  pthread_mutex_lock(&mutex_cfrac);/*Update the counts*/
-  for (i = 0; i < nblocks; i++) (data -> found)[i] |= localfound[i];
-  pthread_mutex_unlock(&mutex_cfrac);
   free(dens);
-  free(localfound);
-  free(bitswap);
   return NULL;
 }
 
